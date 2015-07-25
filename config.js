c3bce4acc396bd4f86fa4b0b6c2da4eaa27cc53b
Umran Hussein <umran.hussein@gmail.com>
2015-07-26 00:25:17 +0500

    updated config defaults

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ var config = {};
 
 config.redis = {}
 
-//redis host -- this is a string
+// redis host -- this is a string
 config.redis.host = '127.0.0.1';
 
-//redis port -- this is an integer
+// redis port -- this is an integer
 config.redis.port = 6379;
 
-//path to mongodb database -- this is a string
-config.mongoServer = 'mongodb://localhost/dummy';
+// path to mongodb database -- this is a string
+config.mongoServer = 'mongodb://localhost/database';
 
-//path to elastic search server or multiple servers -- this is an array containing strings which must be comma separated
+// path to elasticsearch server(s) -- this is an array containing strings which must be comma-separated
 config.elasticServer = ['localhost:9200'];
 
 module.exports = config;
